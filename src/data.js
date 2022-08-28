446b0358d888a57c74179d90a669f9e2065f7a86
Dorian <info@dorianm.com>
2022-08-28 13:23:38 +0200

    init setup

---
+++ src/data.js
@@ -0,0 +1,44 @@
+const data = [
+  {
+    provider: 'Provider2',
+    price: 22.5,
+    maxClusters: 6,
+    memory: 1000,
+    info: 'infoinfo info infoinfoinfo info infoinfo info infoinfoinfo infoinfo'
+  },
+  {
+    provider: 'Provider3',
+    price: 31.5,
+    maxClusters: 7,
+    memory: 1024,
+    info: 'infoinfo info infoinfoinfo info infoinfo info infoinfoinfo infoinfo'
+  },
+  {
+    provider: 'Provider4',
+    price: 20,
+    maxClusters: 3,
+    memory: 512,
+    info: 'infoinfo info infoinfoinfo info infoinfo info infoinfoinfo infoinfo'
+  },
+  {
+    provider: 'Provider5',
+    price: 24,
+    maxClusters: 6,
+    memory: 800,
+    info: 'infoinfo info infoinfoinfo info infoinfo info infoinfoinfo infoinfo'
+  },
+  {
+    provider: 'Provider6',
+    price: 30,
+    maxClusters: 8,
+    memory: 1000,
+    info: 'infoinfo info infoinfoinfo info infoinfo info infoinfoinfo infoinfo'
+  },
+  {
+    provider: 'Provider7',
+    price: 30,
+    maxClusters: 6,
+    memory: 1500,
+    info: 'infoinfo info infoinfoinfo info infoinfo info infoinfoinfo infoinfo'
+  }
+]
