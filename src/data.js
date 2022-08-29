22c4e5d99aee57bf0045cc0717088f52cf028925
Dorian <info@dorianm.com>
2022-08-29 14:32:25 +0200

    simplified prop names

--- src/data.js
+++ src/data.js
@@ -1,44 +1,59 @@
 const data = [
   {
-    provider: 'Provider2',
+    id: '111',
+    name: 'Provider1',
+    price: 29.9,
+    clusters: 6,
+    memory: 1500,
+    info: 'infoinfo info infoinfoinfo info infoinfo info infoinfoinfo infoinfo'
+  },
+  {
+    id: '222',
+    name: 'Provider2',
     price: 22.5,
-    maxClusters: 6,
+    clusters: 6,
     memory: 1000,
     info: 'infoinfo info infoinfoinfo info infoinfo info infoinfoinfo infoinfo'
   },
   {
-    provider: 'Provider3',
+    id: '333',
+    name: 'Provider3',
     price: 31.5,
-    maxClusters: 7,
+    clusters: 7,
     memory: 1024,
     info: 'infoinfo info infoinfoinfo info infoinfo info infoinfoinfo infoinfo'
   },
   {
-    provider: 'Provider4',
+    id: '444',
+    name: 'Provider4',
     price: 20,
-    maxClusters: 3,
+    clusters: 3,
     memory: 512,
     info: 'infoinfo info infoinfoinfo info infoinfo info infoinfoinfo infoinfo'
   },
   {
-    provider: 'Provider5',
+    id: '555',
+    name: 'Provider5',
     price: 24,
-    maxClusters: 6,
+    clusters: 6,
     memory: 800,
     info: 'infoinfo info infoinfoinfo info infoinfo info infoinfoinfo infoinfo'
   },
   {
-    provider: 'Provider6',
+    id: '666',
+    name: 'Provider6',
     price: 30,
-    maxClusters: 8,
+    clusters: 8,
     memory: 1000,
     info: 'infoinfo info infoinfoinfo info infoinfo info infoinfoinfo infoinfo'
   },
   {
-    provider: 'Provider7',
+    id: '777',
+    name: 'Provider7',
     price: 30,
-    maxClusters: 6,
+    clusters: 6,
     memory: 1500,
     info: 'infoinfo info infoinfoinfo info infoinfo info infoinfoinfo infoinfo'
   }
 ]
+export default data;
